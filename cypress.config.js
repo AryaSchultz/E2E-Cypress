f75c4d2fcd2dbe961eca8f28c37ca49b328a341a
AryaSchultz <aryschulltz@gmail.com>
2023-11-21 16:05:14 -0300

    fazendo comando persoanlizado

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  projectId: 'azkida',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/loginCorreto.cy.js
+++ cypress/e2e/loginCorreto.cy.js
@@ -1,8 +1,10 @@
 describe('Pagina de Login', () => { //nome do teste 
-    it('Verificar quando colocado usuarios corretos login está sendo feito', () => { //Colocar uma descrição do que o teste deve fazer
-      cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home')
-      cy.get('[data-test="loginUserName"]').type('carowl');
-      cy.get('[data-test="loginPassword"]').type('12345678');
-      cy.contains('button','login').click();
-    })
+  beforeEach (() =>{
+    cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home')
   })
+
+    it('Verificar quando colocado usuarios corretos login está sendo feito', () => { //Colocar uma descrição do que o teste deve fazer
+        
+    cy.login('catarinap','catarina123');
+    })
+})
