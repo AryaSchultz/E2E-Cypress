dcb8c0c323c54d9b91768badd63213a6e18db522
AryaSchultz <aryschulltz@gmail.com>
2023-12-11 17:17:33 -0300

    fazendo so stubpost no login incorreto

--- cypress/e2e/loginErrado.cy.js
+++ cypress/e2e/loginErrado.cy.js
@@ -1,12 +1,16 @@
 describe('Pagina de Login', () => { //nome do teste 
     it('Verificar quando colcoamos dados errados o login é imepedido', () => { //Colocar uma descrição do que o teste deve fazer
       cy.visit('https://3076-cypress-alurapic-front.vercel.app/#/home')
-      cy.get('[data-test="loginUserName"]').type('testeerro');
-      cy.get('[data-test="loginPassword"]').type('12345678w');
-      cy.contains('button','login').click();
-      cy.on('window:alert', (str) => {
-        expect(str).to.equal('Invalid user name or password')
+   
+      cy.intercept('POST', 'https://alurapic-api.onrender.com/user/login',{
+        statusCode:400
+      }).as('stubPost')
+
+      it('Devefalhar mesmo que os campos sejam preechidos corretamente', () =>{
+        cy.login('carowl','12345678');
+        cy.wait('@stubPost');
       })
+
 
     })
   })
